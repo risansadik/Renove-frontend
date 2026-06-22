aa37dc3b8749ceaf7c776aeb652ec8a5b4620919
risansadik <risansadik@gmail.com>
2026-06-23 00:33:05 +0530

    fix socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, type Socket } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
+const SOCKET_URL =  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5000";
 
 const socket: Socket = io(SOCKET_URL, {
   withCredentials: true,
