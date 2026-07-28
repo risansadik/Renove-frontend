f2f532ffe056bbea35b2d4cdfa5a516c7532ff40
Muhammed Risan Bin Sadik <Risan Bin Sadik@Binsadik.BinSadique>
2026-07-28 12:16:40 +0530

    feat(devops): add Docker support for development and production

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
